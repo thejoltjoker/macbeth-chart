6545e2bd27ebbdcdc608c1b9043f451ec2eb0d44
Johannes <hello@thejoltjoker.com>
2025-08-22 08:26:57 +0200

    chore: update Vite configuration to set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({ plugins: [tailwindcss()] });
+export default defineConfig({
+  plugins: [tailwindcss()],
+  base: "/macbeth-chart/",
+});
